5759a9f9519ded36a8dd0427a2b08872aacbe281
Eduardo Donini <eduardodonini1311@gmail.com>
2024-01-29 19:38:16 -0300

    Crud Scaffolding

--- SalesProject/Startup.cs
+++ SalesProject/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesProject.Data;
 
 namespace SalesProject
 {
@@ -33,6 +35,9 @@ namespace SalesProject
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesProjectContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesProjectContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
