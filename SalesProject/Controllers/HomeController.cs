1ba7882d9d0ccf7211611b96e4d876a95801c1a8
Eduardo Donini <eduardodonini1311@gmail.com>
2024-01-29 18:33:15 -0300

    First-Model-Controllers-View

--- SalesProject/Controllers/HomeController.cs
+++ SalesProject/Controllers/HomeController.cs
@@ -37,7 +37,7 @@ namespace SalesProject.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new SalesProject.Models.ViewModels.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
