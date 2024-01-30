f04741f4822b7fbdeac66dd27e7ab3ef9a5e7fec
Eduardo Donini <eduardodonini1311@gmail.com>
2024-01-30 20:03:51 -0300

    Update Seller and custom service exception

--- SalesProject/Controllers/SellersController.cs
+++ SalesProject/Controllers/SellersController.cs
@@ -2,6 +2,7 @@
 using SalesProject.Models;
 using SalesProject.Models.ViewModels;
 using SalesProject.Services;
+using SalesProject.Services.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -78,6 +79,47 @@ namespace SalesProject.Controllers
             }
 
             return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit (int id, Seller seller)
+        {
+            if(id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
