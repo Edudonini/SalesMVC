b4a0f89e6fcd8d34dc0677401d2648f0473a86fc
Eduardo Donini <eduardodonini1311@gmail.com>
2024-01-29 21:16:15 -0300

    Add Outher Entitites

--- SalesProject/Data/SalesProjectContext.cs
+++ SalesProject/Data/SalesProjectContext.cs
@@ -15,5 +15,7 @@ namespace SalesProject.Data
         }
 
         public DbSet<SalesProject.Models.Department> Department { get; set; }
+        public DbSet<Seller> Seller  { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
---
+++ SalesProject/Models/Enums/SalesStatus.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace SalesProject.Models.Enums
+{
+    public enum SalesStatus : int
+    {
+        Pendind = 0,
+        Billed = 1,
+        Canceled = 2
+    }
+}
